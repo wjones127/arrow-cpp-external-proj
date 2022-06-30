87b01cd4edc10a8faad4cc8a241accc8ed6b595d
Will Jones <willjones127@gmail.com>
2022-06-29 18:07:23 -0700

    Use flight example instead

--- cmake_modules/Arrow.CMakeLists.txt.cmake
+++ cmake_modules/Arrow.CMakeLists.txt.cmake
@@ -14,6 +14,8 @@ set(ARROW_CMAKE_ARGS
     -DARROW_S3=OFF
     -DARROW_CSV=ON
     -DARROW_FLIGHT=ON
+    -DARROW_PARQUET=ON
+    -DARROW_COMPUTE=ON
 )
 
 include(ExternalProject)
@@ -21,6 +23,8 @@ ExternalProject_Add(
     arrow_ep 
     GIT_REPOSITORY https://github.com/apache/arrow.git
     GIT_TAG master
+    # GIT_REPOSITORY https://github.com/wjones127/arrow.git
+    # GIT_TAG bundle-flight
     SOURCE_SUBDIR cpp
     CONFIGURE_COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" ${ARROW_CMAKE_ARGS} "${CMAKE_CURRENT_BINARY_DIR}/arrow_ep-prefix/src/arrow_ep/cpp/"
     INSTALL_COMMAND DESTDIR=${CMAKE_CURRENT_BINARY_DIR}/arrow_ep-prefix/src/arrow_ep-install cmake --build . --target install
